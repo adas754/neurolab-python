bc512e3a1bc314abd33f53e6d6384c0b8de5bc1d
adas754 <anweshadas754@gmail.com>
2023-08-22 22:49:46 +0530

    password_generetor

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('Hello World')
+from src.pass_gen import password_generator
+
+if __name__=="__main__":
+    min_length=int(input("Enter the minimum length :"))
+    number =input("Do you want to have number(y/n)? ").lower()=='y'
+    special_character =input("Do you want to have special_character (y/n)? ").lower()=='y'
+    my_pass=password_generator(min_length=min_length,number=number,special_character=special_character)
+    print(my_pass)
+``
